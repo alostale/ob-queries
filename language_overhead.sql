d1798e5cdd11e21909a2fdfbb0de6d62c63b37bd
Asier Lostalé <asier.lostale@openbravo.com>
2017-12-12 13:47:47 +0100

    Create language_overhead.sql

---
+++ language_overhead.sql
@@ -0,0 +1,5 @@
+select '0. ALL' as type, sum(calls) calls, sum(total_time) total_time, '-' as query    FROM pg_stat_statements 
+union
+select '1. AD_LANGUAGE', sum(calls) calls, sum(total_time) total_time, substring(trim(query),0,100) as qry from pg_stat_statements where query like 'select adlanguage__.AD_Language_ID as AD1_55_0_%from AD_Language%'
+group by query
+order by 1,4;
